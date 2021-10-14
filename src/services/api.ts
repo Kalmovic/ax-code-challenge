ec8fe7b4613afd6f0e3becf0a1cd0fb3fb6bb20e
Kalmovic <joseph.kaltenecker@gmail.com>
2021-10-14 09:55:49 -0300

    add api calls

---
+++ src/services/api.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://testapp.axreng.com:3000/',
+    headers: {'Content-Type': 'application/json'}
+})
+
+export async function getKeywordResult(identification : string) {
+    return await api.get(`/crawl/${identification}`)
+}
+
+export async function getId(keyword : string) {
+    return await api.post("/crawl", {
+        keyword
+    })
+}
