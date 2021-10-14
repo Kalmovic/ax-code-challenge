df263083a21e0be1ab207c23d87b66f0365ebb94
Kalmovic <joseph.kaltenecker@gmail.com>
2021-10-14 09:54:55 -0300

    dashboard component

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,119 @@
+import React from 'react';
+import { getId, getKeywordResult } from '../../services/api';
+import Button from '../Button';
+import Input from '../Input';
+import { ButtonWrapper, Content, DetailsWrapper, ErrorMessage, ExpandSpan, FoundUrl, InputWrapper, Keyword, NotFound, ResultItem, Results, SeachKeyStatus, SearchLabel, Tag, UrlList } from './styles';
+import { IResult } from './types';
+
+const Dashboard: React.FC = () => {
+    const [result, setResult] = React.useState<IResult[]>([]);
+    const [search, setSearch] = React.useState("");
+    const [errorMessage, setErrorMessage] = React.useState("");
+
+    const getResult = async (identification:string) => {
+        const responseStatus = await getKeywordResult(identification);
+
+        const { id, status, urls } :any = responseStatus.data;
+        
+        let newResult = [];
+        newResult = [...result , {
+            id,
+            status,
+            urls,
+            keyword: search,
+            expanded: false,
+        }];
+        setResult(newResult);
+    }
+
+    const checkAgain = async (identification:string) => {
+        const responseStatus = await getKeywordResult(identification);
+
+        const { id, status, urls } :any = responseStatus.data;
+        
+        let newResult = result.map(item => {
+            if(item.id === id) {
+                item.status = status;
+                item.urls = urls;
+            }
+            return item;
+        });
+        setResult(newResult)
+    }
+
+    const verifyInput = (search: string) => {
+        if(search.length < 4) {
+            setErrorMessage("Keyword must have more than 3 characters");
+            return;
+        } else if(search.length > 32) {
+            setErrorMessage("Keyword must have less than 33 characters");
+            return;
+        } else {
+            searchKeyWord(search);
+            errorMessage !== "" && setErrorMessage("");
+        }
+    }
+
+    const searchKeyWord = async (keyword: string) => {
+        setSearch("");
+        const responseId = await getId(keyword);
+        const { id } :any = responseId.data;
+        
+        getResult(id);
+    };
+
+    const changeExpanded = (id: number) => {
+        let newResult = result.map((item, index) => {
+            if(index === id) item.expanded = !item.expanded;
+            return item;
+        });
+        console.log(newResult);
+        
+        setResult(newResult);
+    }
+
+    return <Content> 
+          <InputWrapper>
+            <Input name="Search" value={search} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)} placeholder="Search for keywords" />
+            <Button onClick={() => verifyInput(search)}>
+                <SearchLabel>Search</SearchLabel>
+            </Button>
+          </InputWrapper>
+          {errorMessage ? (<ErrorMessage>{errorMessage}</ErrorMessage>) : null}
+          {result.length > 0 && <Results className="results">
+              {result.map((item, index) => {
+                  return (
+                      <React.Fragment key={index}>
+                        <ResultItem className="result-item" key={index}>
+                          <SeachKeyStatus>
+                          <Tag status={item.status}>{item.status.toUpperCase()}</Tag>
+                          <Keyword>{item.keyword}</Keyword>
+                          </SeachKeyStatus>
+                          <ExpandSpan onClick={() => changeExpanded(index)}>{item.expanded ? "hide urls" : "show urls"}</ExpandSpan>
+                          </ResultItem>
+                          {item.expanded ? (
+                              <DetailsWrapper>
+                                    <UrlList>
+                                  {item.urls.length > 0 ? 
+                                  item.urls.map((url, index) => {
+                                      return (
+                                          <FoundUrl className="found-url" key={index}>{url}</FoundUrl>
+                                      )
+                                  }) : <NotFound>Nenhum resultado encontrado.</NotFound>}
+                              </UrlList>
+                              {item.status === 'active' ? <ButtonWrapper>
+                                  <Button onClick={() => checkAgain(item.id)}>
+                                    <SearchLabel>Chech Again</SearchLabel>
+                                  </Button>
+                              </ButtonWrapper> : null}
+                              </DetailsWrapper>                              
+                          ) : null}
+                      </React.Fragment>
+                      
+                  )
+              })}
+          </Results>}
+      </Content>;
+}
+
+export default Dashboard;
---
+++ src/components/Dashboard/styles.ts
@@ -0,0 +1,151 @@
+import styled, { css, keyframes } from 'styled-components';
+
+const opacityChange = keyframes`
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
+`
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 200px auto 0;
+  max-width: 400px;
+`;
+
+export const InputWrapper = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: var(--white);
+  border: 1px solid var(--purple);
+  border-radius: 26px;
+  padding: 12px 12px 12px 24px;
+  width: 100%;
+`;
+
+export const ErrorMessage = styled.span`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  color: var(--warning);
+
+  margin-top: 10px;
+`;
+
+export const SearchLabel = styled.span`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  color: var(--white);
+`;
+
+export const Results = styled.div`
+  margin-top: 15px;
+  display: flex;
+  flex-direction: column;
+  background: var(--white);
+  border-radius: 10px;
+  padding: 10px;
+  width: 100%;
+`;
+
+export const ResultItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: var(--background);
+  border-radius: 5px 0px 0px 5px;
+
+  & + .result-item {
+    margin-top: 10px;
+  }
+
+  animation: ${opacityChange} 0.2s linear forwards;
+`;
+
+export const SeachKeyStatus = styled.div`
+  display: flex;
+`;
+
+export const ExpandSpan = styled.span`
+  color: var(--purple);
+  text-align: center;
+  text-decoration: underline;
+
+  margin-right: 8px;
+
+  :hover{
+    cursor: pointer;
+  }
+`;
+
+export const Keyword = styled.span`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 25px;
+  color: var(--black);
+
+  margin-left: 8px;
+`;
+export const UrlList = styled.ul`
+  margin: 10px 0;
+  display: flex;
+  flex-direction: column;
+`;
+export const FoundUrl = styled.span`
+  color: var(--black);
+  word-break: break-all;
+  padding: 5px;
+
+  :nth-child(even) {
+    background-color: var(--yellow-light);
+  }
+
+  & + .found-url {
+    padding-top: 5px;
+  }
+
+  animation: ${opacityChange} 0.4s linear forwards;
+
+`;
+
+export const NotFound = styled.span`
+  color: var(--black);
+  text-align: center;
+  margin-top: 10px;
+`;
+
+export const DetailsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin-bottom: 12px;
+
+  animation: ${opacityChange} 0.4s linear forwards;
+`;
+
+export const ButtonWrapper = styled.div`
+  margin-top: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+interface TagProps {
+  status: string
+}
+
+export const Tag = styled.div<TagProps>`
+  width: 70px;
+  padding: 2px 4px;
+  background: ${props => props.status === 'active' ? 'var(--orange)' : 'var(--green)'};
+  border-radius: 5px 0 0px 5px;
+  text-align: center;
+`;
